6ac6420484faa60adcf6e03a0d765794f5d87dcc
heyuxian <heyuxian.me@gmail.com>
2019-01-21 20:39:10 +0800

    spring boot 入门

---
+++ demo01/src/main/java/com/docs4dev/springboot/demo01/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.docs4dev.springboot.demo01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "hello world!";
+    }
+}
